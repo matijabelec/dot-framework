e2f1b9c160093659708f0d751e737ab9232ad8fe
matijabelec <mbelec@foi.hr>
2015-07-24 11:00:56 +0200

    removed echo

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
 
+/* declare ROOT directories */
 define('ROOT', __DIR__);
 define('ROOT_FW', ROOT.'/fw');
 define('ROOT_APP', ROOT.'/app');
 
+/* load Dot framework */
 include_once(ROOT_FW.'/belfw.php');
-echo ROOT_FW;
 
+
+/* load app configurations */
 include_once(ROOT_APP.'/config.php');
+
+
+/* run app */
 include_once(ROOT_APP.'/routes.php');
 Router::run();
 
