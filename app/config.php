cd30e023b98e107067a38503194f6c8b7f92558a
matijabelec <mbelec@foi.hr>
2015-07-26 19:41:50 +0200

    updated app/config to include DEFAULT_URL_KEY, updated 'index.php' to run safe functions

--- app/config.php
+++ app/config.php
@@ -7,14 +7,21 @@ define('SITE_CSS', WEB_SITE.'/css');
 define('SITE_JS', WEB_SITE.'/js');
 define('SITE_GFX', WEB_SITE.'/gfx');
 
+
 /*** database ***/
 define('DFW_DB_DATABASE', 'recenicadvije');
 define('DFW_DB_HOSTNAME', 'localhost');
 define('DFW_DB_USERNAME', 'root');
 define('DFW_DB_PASSWORD', 'belec');
 
+
+/*** default key for ?key=$url (.htaccess file) ***/
+define('DEFAULT_URL_KEY', 'url');
+
+
 /*** lang ***/
 define('DEFAULT_LANG', 'en');
+
 
 /*** error reporting ***/
 error_reporting(E_ALL);
--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
 
-/* declare ROOT directories */
+/*** declare ROOT directories ***/
 define('ROOT', __DIR__);
 define('ROOT_FW', ROOT.'/fw');
 define('ROOT_APP', ROOT.'/app');
 
-/* load Dot framework */
+
+/*** load Dot framework ***/
 include_once(ROOT_FW.'/belfw.php');
 
 
-/* run app */
+/*** include routes ***/
 include_once(ROOT_APP.'/routes.php');
+
+/*** init ***/
+spl_autoload_register('the_autoloader');
+remove_mq();
+unregister_globals();
+
 Router::run();
 
 ?>
