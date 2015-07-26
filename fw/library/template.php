0956c9b2f239fb69a0b1b67f404c8876039a2a46
matijabelec <mbelec@foi.hr>
2015-07-27 00:10:19 +0200

    totaly replaced fw/library/template

--- fw/library/template.php
+++ fw/library/template.php
@@ -1,124 +1,239 @@
 <?php
 
+/**
+ * Template class used for templates in Dot-framework
+ *
+ * Template class used for templates in Dot-framework
+ *
+ * PHP version 5
+ *
+ * LICENSE: 
+ *
+ * @author     Matija Belec <matijabelec1@gmail.com>
+ * @copyright  2015 Matija Belec
+ * @license    Proprietary
+ */
+
 class Template {
-    protected $data = '';
-    protected $filled_data = '';
+    private $template;
+    private $data;
+    private $n;
+    private $curr_n;
     
-    /* 
-     * values:
-     *        key => value:string
-     *        key => array(view_name:string,
-     *                     values:array)
+    /**
+     * Constructor for template
+     * 
+     * Set template string (load string from file) or set string inline.
+     * 
+     * @param string    $arg1 an string for name of template, or template 
+     *                      string itself
+     * @param bool    $arg2 if inline is set to false then name is template 
+     *                      name, or, if inline is set to true, than name is
+     *                      template string
+     * 
+     * @access public
      */
-    protected $values = array();
+    public function __construct($name, $inline=false) {
+        $this->template = '';
+        
+        if(isset($name) ) {
+            if($inline != false) {
+                $this->template = $name;
+            } else {
+                $this->template = Loader::get_template($name);
+            }
+            if(isset($this->template) ) {
+                $this->process_hardcode_includes();
+            }
+        }
+        
+        $this->data = array();
+        
+        $this->n = 1;
+    }
     
-    private $safe;
+    /**
+     * Sets value for key
+     * 
+     * Set value for key. If template repetition is set (with method repeat
+     * then third argument is used to determine for which template in 
+     * repeatition sets key)
+     * 
+     * @param string    $arg1 an string which is used to determine a key on 
+     *                      template
+     * @param string    $arg2 an string which is used as value which is 
+     *                      filled in where key is on template located
+     * @param int    $arg3 an integer which identifies template number if 
+     *                  method repeat is used
+     * 
+     * @access public
+     */
+    public function set($key, $val, $arr_id=1) {
+        if(isset($key) && isset($val) ) {
+            $a = &$this->data[$arr_id];
+            $a[$key] = array('type'=>'value', 'value'=>$val);
+        }
+    }
     
-    protected function process_includes() {
-        $string = $this->data;
-        $pattern = '/\{#include\(([a-zA-Z0-9-\/]*)\)\}/i'; /*{#include(region/nav)}*/
-        $this->data = preg_replace_callback(
+    /**
+     * Sets data (multiple values and keys) in template
+     * 
+     * Set multiple values for multiple keys in one function call. Array
+     * must be like: array([key=>val] [, key2=>val2] [, ...]).
+     * 
+     * @param array    $arg1 an array which includes all key=>value pairs
+     * @param int    $arg3 an integer which identifies template number if 
+     *                  method repeat is used
+     * 
+     * @access public
+     */
+    public function set_data($data, $arr_id=1) {
+        if(isset($data) && is_array($data) ) {
+            foreach($data as $key=>$val) {
+                $a = &$this->data[$arr_id];
+                $a[$key] = array('type'=>'value', 'value'=>$val);
+            }
+        }
+    }
+    
+    /**
+     * Set number of repeatition of current template on method output call
+     * 
+     * On method output call class will create n copies of template
+     * 
+     * @param int    $arg1 an integer of how many templates will be created.
+     * 
+     * @access public
+     */
+    public function repeat($n) {
+        if(isset($n) && is_numeric($n) && $n>1) {
+            $this->n = $n;
+        }
+    }
+    
+    /**
+     * Returns filled data
+     * 
+     * Method output fills n copies of template and returns filled template string
+     * 
+     * @param bool    $arg1 an true/false value. if true than all {@key} found
+     *                      in template but not in data will be replaced with
+     *                      empty string or, if false, key will be left intact
+     * 
+     * @return string the filled template.
+     * @access public
+     */
+    public function output($safe=true) {
+        $string = $this->template;
+        $pattern = '/\{\@([a-zA-Z0-9-]*)\}/i'; //{@key}
+        
+        if($safe == true) {
+            $filled = preg_replace_callback(
+                $pattern,
+                function($matches) {
+                    $value = $this->filled_data($matches[1]);
+                    if(is_null($value) ) {
+                        return '';
+                    }
+                    return $value;
+                },
+                $string
+            );
+            for($i=2; $i<=$this->n; $i++) {
+                $this->curr_n = $i;
+                $filled .= preg_replace_callback(
+                    $pattern,
+                    function($matches) {
+                        $value = $this->filled_data($matches[1], $this->curr_n);
+                        if(is_null($value) ) {
+                            return '';
+                        }
+                        return $value;
+                    },
+                    $string
+                );
+            }
+        } else {
+            $filled = preg_replace_callback(
+                $pattern,
+                function($matches) {
+                    $value = $this->filled_data($matches[1]);
+                    if(is_null($value) ) {
+                        $v = $matches[0];
+                        return $v;
+                    }
+                    return $value;
+                },
+                $string
+            );
+            for($i=2; $i<=$this->n; $i++) {
+                $this->curr_n = $i;
+                $filled .= preg_replace_callback(
+                    $pattern,
+                    function($matches) {
+                        $value = $this->filled_data($matches[1], $this->curr_n);
+                        if(is_null($value) ) {
+                            return '';
+                        }
+                        return $value;
+                    },
+                    $string
+                );
+            }
+        }
+        
+        return $filled;
+    }
+    
+    /**
+     * Adds new template as value for key in template
+     * 
+     * This method adds new template in this template which is then saved as
+     * value for key
+     * 
+     * @param string    $arg1 an key in template
+     * @param Template    $arg2 an template which is set as value
+     * @param int    $arg3 used if repeat() function is called to set template
+     *                  on n-th template in copies
+     * 
+     * @return string the filled template.
+     * @access public
+     */
+    public function include_template($key, Template $template, $arr_n=1) {
+        if(isset($key) && isset($template) ) {
+            $a = &$this->data[$arr_id];
+            $a[$key] = array('type'=>'template', 'template'=>&$template);
+        }
+    }
+    
+    
+    private function process_hardcode_includes() {
+        $string = $this->template;
+        $pattern = '/\{#include\(([a-zA-Z0-9-\/]*)\)\}/i'; //{#include(region/nav)}
+        $this->template = preg_replace_callback(
             $pattern,
             function($matches) {
                 $value = $matches[1];
                 if(is_null($value) )
                     return '';
-                
                 $tpl = new Template($value);
-                
                 return $tpl->output();
             },
             $string
         );
     }
     
-    public function __construct($name, $inline=false) {
-        $this->set('ROOT', WEB_ROOT);
-        $this->set('SITE', WEB_SITE);
-        
-        if($inline != false) {
-            $this->data = $name;
-        } else {
-            $this->data = Loader::get_template($name);
-        }
-        
-        $this->filled_data = $this->data;
-        
-        if(!is_null($this->data) ) {
-            $this->process_includes();
-        }
-    }
-    
-    public function set($key, $value) {
-        $this->values[$key] = $value;
-    }
-    
-    public function set_data($data) {
-        if(isset($data) && is_array($data) )
-            foreach($data as $k=>$v)
-                $this->values[$k] = $v;
-    }
-    
-    public function get($key) {
-        if(isset($this->values[$key]) )
-            return $this->values[$key];
-        return null;
-    }
-    
-    public function fill($safe=true, $rewrite=false) {
-        $this->safe = $safe;
-        $string = $this->data;
-        $pattern = '/\{\@([a-zA-Z0-9-]*)\}/i'; /*{@key}*/
-        $this->filled_data = preg_replace_callback(
-            $pattern,
-            function($matches) {
-                $value = $this->get($matches[1]);
-                if(is_null($value) ) {
-                    $v = ($this->safe==true ? '' : $matches[0]);
-                    return $v;
+    private function filled_data($key, $arr_id=1) {
+        if(isset($key) && isset($this->data[$arr_id]) ) {
+            $a = &$this->data[$arr_id];
+            if(isset($a[$key]) ) {
+                $item = &$a[$key];
+                switch($item['type']) {
+                    case 'value': return $item['value'];
+                    case 'template': return $item['template']->output();
+                    default: break;
                 }
-                return $value;
-            },
-            $string
-        );
-        
-        if($rewrite!=false) {
-            $this->data = $this->filled_data;
-        }
-    }
-    
-    public function output() {
-        return $this->filled_data;
-    }
-    
-    
-    
-    private static $merge_i;
-    public static function merge(Template $template, $n) {
-        if(isset($n) && is_numeric($n) && $n>0) {
-            self::$merge_i = 1;
-            $out = '';
-            $string = $template->output();
-            $pattern = '/\{\@([a-zA-Z0-9-]*)\}/i'; /*{@key}*/
-            for($i=1; $i<=$n; $i++) {
-                $data = preg_replace_callback(
-                    $pattern,
-                    function($matches) {
-                        switch($matches[1]) {
-                            case 'ROOT':
-                            case 'SITE':
-                                return $matches[0];
-                            default:
-                                $v = '{@' .  $matches[1] . '-' . self::$merge_i . '}';
-                                return $v;
-                        }
-                    },
-                    $string
-                );
-                $out .= $data;
-                self::$merge_i++;
             }
-            return $out;
         }
         return null;
     }
